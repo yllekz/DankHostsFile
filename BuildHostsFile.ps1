ab72721b607ca78d157067081bd6d8ccb81f264a
yllekz <nothingnothingnothin@icloud.com>
2020-05-26 11:21:24 -0400

    Build Script: -Updated file paths to be relative -Added new BeachBody exception
    Hosts file:
    -Generated updated version

--- BuildHostsFile.ps1
+++ BuildHostsFile.ps1
@@ -1,13 +1,13 @@
 #Build my hosts file
 
-$HostsFile = 'c:\users\steve\Desktop\hosts.txt'
+$HostsFile = "c:\users\$($env:USERNAME)\Desktop\hosts.txt"
 
 #region Put the hosts file together:
 
 $HostsFileContent = @()
 
-$HostsFileContent += Get-Content "D:\GitHub\DankHostsFile\CustomEntriesSK.txt"
-$HostsFileContent += Get-Content "D:\GitHub\DankHostsFile\SpybotEntries.txt"
+$HostsFileContent += Get-Content "$($PSSCRIPTROOT)\CustomEntriesSK.txt"
+$HostsFileContent += Get-Content "$($PSSCRIPTROOT)\SpybotEntries.txt"
 
 $HostsFileContent += "#region########################################################################################################################################"
 $HostsFileContent += "# Start of StevenBlack Hosts"
@@ -93,6 +93,12 @@ $row = new-object PSObject -Property @{
     'Comment'   = "#Unblocked SK 2020/03/15 [BeachBody]";
 }
 $Exceptions += $row
+
+$row = new-object PSObject -Property @{
+    'Entry'     = "0.0.0.0 metrics.teambeachbody.com";
+    'Comment'   = "#Unblocked SK 2020/05/26 [BeachBody]";
+}
+$Exceptions += $row
 #endregion
 
 #Comment out exceptions from above list. TODO: Figure out a way to make this into a loop...
@@ -110,7 +116,8 @@ $9 = $8.replace($Exceptions[8].Entry, "#$($Exceptions[8].Entry) $($Exceptions[8]
 $10 = $9.replace($Exceptions[9].Entry, "#$($Exceptions[9].Entry) $($Exceptions[9].Comment)")
 $11 = $10.replace($Exceptions[10].Entry, "#$($Exceptions[10].Entry) $($Exceptions[10].Comment)")
 $12 = $11.replace($Exceptions[11].Entry, "#$($Exceptions[11].Entry) $($Exceptions[11].Comment)")
-$12|out-file $HostsFile
+$13 = $12.replace($Exceptions[12].Entry, "#$($Exceptions[12].Entry) $($Exceptions[12].Comment)")
+$13|out-file $HostsFile
 
 "# End of entries inserted by StevenBlack hosts ########################################################################################################################################"|out-file $HostsFile -Append
 
