ebb2d9a2f09cd7744eecdd202b5be02f233903a8
yllekz <nothingnothingnothin@icloud.com>
2020-07-28 14:20:53 -0400

    Remove location-specific stuff + new script
    *Repo was made public right before this*
    
    Hosts:
    -Remove LAN-specific names
    
    installhosts:
    -Added installhosts.sh

---
+++ installhosts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#At the moment this script assumes that you have freshly rebooted your DD-WRT router.
+#Right now this script will NOT properly refresh an already-modified hosts file.
+#We also assume /tmp/hosts is blank except for the default entries that are automatically added.
+
+#region pull hosts and append to hosts file:
+
+temphosts=~/hosts.txt
+prodhosts=/tmp/hosts
+
+#This is a work in progress to attempt to clear out the ...
+#while read line; do if [ "$line" = "10.*" ]; then echo "$line"; else; fi; done < $prodhosts;
+#while read line; do if [ "$line" = "10.*" ]; then echo "$line" >> $prodhosts; else; fi; done < $prodhosts;
+
+wget -O $temphosts https://raw.githubusercontent.com/yllekz/DankHostsFile/master/hosts;
+while read line; do echo "$line" >> $prodhosts; done < $temphosts;
+killall -HUP dnsmasq
+
+#endregion
