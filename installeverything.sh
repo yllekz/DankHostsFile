8d5cead069bc2f7124e0bee71bc7b985997488a0
yllekz <nothingnothingnothin@icloud.com>
2020-08-04 10:05:37 -0400

    Execution line fixes

--- installeverything.sh
+++ installeverything.sh
@@ -5,9 +5,11 @@ sleep 300; #Grace period in case you get locked uot of the router.
 #Pull hosts file script and run it
 hostsscript=/tmp/root/installhosts.sh
 wget -O $hostsscript https://raw.githubusercontent.com/yllekz/DankHostsFile/master/installhosts.sh;
-./$hostsscript
+chmod +x $hostsscript
+sh $hostsscript
 
 #Pull ipsum script and run it
 ipsumscript=/tmp/root/ipsum.sh
 wget -O $ipsumscript https://raw.githubusercontent.com/yllekz/DankHostsFile/master/ipsum.sh;
-./$ipsumscript
+chmod +x $ipsumscript
+sh $ipsumscript
