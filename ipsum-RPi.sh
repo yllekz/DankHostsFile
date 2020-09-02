d26f4b0fc9a97744eac0fc8c24fa01762210ae62
yllekz <nothingnothingnothin@icloud.com>
2020-09-02 07:49:38 -0400

    Added an IP
    -Also did some minor comment cleanup

--- ipsum-RPi.sh
+++ ipsum-RPi.sh
@@ -8,16 +8,16 @@ echo "Removing previous entries first..."
 
 #Purge INPUT table:
 sudo iptables -F INPUT
+#endregion
 
 #region Now add entries:
 echo $(date)
 echo "Adding entries to iptables..."
 
-#region Custom entries SK
+#Custom entries SK
 for ip in $(curl --compressed https://raw.githubusercontent.com/yllekz/DankHostsFile/master/IPBanList.txt 2>/dev/null | grep -v "#");
     do sudo iptables -I INPUT -s $ip -j DROP;
 done;
-#endregion
 
 #ipsum list:
 for ip in $(curl --compressed https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1);
@@ -28,10 +28,9 @@ done;
 for ip in $(curl --compressed https://raw.githubusercontent.com/yllekz/DankHostsFile/master/CountryBlacklist.txt 2>/dev/null | grep -v "#");
     do sudo iptables -I INPUT -s $ip -j DROP;
 done;
-#endregion
+#endregion end of entry additions
 
 echo $(date)
-#endregion
 
 ####Display for confirmation:
 #sudo iptables -vnL INPUT --line-numbers|more
